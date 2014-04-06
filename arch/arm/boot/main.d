46a840585ae55fe78eef3c2feb97c2a8662bcbb1
UVa student <student@student-VirtualBox.(none)>
2014-04-06 16:37:08 -0400

    new stuff

--- arch/arm/boot/main.d
+++ arch/arm/boot/main.d
@@ -1,1 +1,2 @@
-boot/main.o: boot/main.bc boot/main.s
+boot/libmain-7ff4ebdb-0.1.so: ../../lib.rs ../../kernel/mod.rs ../../kernel/int.rs ../../kernel/ptr.rs ../../kernel/memory/mod.rs ../../kernel/memory/allocator.rs ../../kernel/memory/physical.rs ../../kernel/memory/virtual.rs ../../kernel/sgash.rs ../../kernel/rt.rs ../../rust-core/support.rs ../../arch/arm/cpu/mod.rs ../../arch/arm/cpu/interrupt.rs ../../arch/arm/cpu/mmu.rs ../../arch/arm/io/mod.rs ../../arch/arm/io/font.rs ../../arch/arm/drivers/mod.rs
+
